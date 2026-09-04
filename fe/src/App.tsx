ed644aebdc0673427d4af374bb043a510b2b9d82
dandrok <patryk.stusik@gmail.com>
2026-09-04 12:14:02 +0200

    refactor(fe): extract FilterItem and improve grid responsiveness

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -5,7 +5,7 @@ import { FiltersProvider } from './contexts/filters';
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <div className="container mx-auto max-w-5xl">
+      <div className="container mx-auto max-w-5xl px-2">
         <FiltersProvider>
           <Filters />
           <Products />
--- fe/src/components/products/index.tsx
+++ fe/src/components/products/index.tsx
@@ -32,7 +32,7 @@ export const Products = () => {
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-x-4 gap-y-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center">
         {products.map((product) => (
           <ProductCard key={product.code} {...product} />
         ))}
